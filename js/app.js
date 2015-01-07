041333495c9b55ac362e64058c83d3dc76610d8d
William Andres Giraldo Rincon <williamagrincon@gmail.com>
2015-01-07 18:54:37 -0500

    Se añaden built-in directives para añadir funcionalidad a los botones

--- js/app.js
+++ js/app.js
@@ -2,13 +2,24 @@
 	var app = angular.module('store',[]);
 
 	app.controller('StoreController',function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
+	var gems = [
+	 {
 		name: 'Ruby',
 		price: 295.000,
 		description: 'Gema hermosa',
+		canPurchase: false,
+		soldOut: false
+	},
+	{
+		name: 'Diamante',
+		price: 595.000,
+		description: 'Gema hermosa para dama',
+		canPurchase: false,
+		soldOut: true
 	}
+	];
 })();
 
